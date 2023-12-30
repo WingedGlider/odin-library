1937ed8f76234bcb91f41ab816cbcdd46a70fc8e
WingedGlider <william.armstrong967@gmail.com>
2023-12-30 05:49:02 -0500

    Add form preview, genre color classes, shelve function

--- script.js
+++ script.js
@@ -5,14 +5,17 @@ Books.push(new book("Caesar and Christ", "Will Durant", 752, "History", true));
 Books.push(new book("Dune", "Frank Herbert", 412, "Sci-Fi", true));
 let deskBook = document.querySelector('.desk').innerHTML;
 let shelfBook = document.querySelector('.shelf-track').innerHTML;
-document.querySelector('.shelf-track').innerHTML = "";
-document.querySelector('.desk').innerHTML = "";
-let deskButtons = document.querySelectorAll('button');
+formInit();
+shelve(Books);
 
-
-deskButtons.forEach((button) => {
+document.querySelectorAll('button').forEach((button) => {
     button.addEventListener('click', ()=>{
-        if(button.innerHTML.includes('open')) if(document.querySelector('.desk').innerHTML == "") document.querySelector('.desk').innerHTML = deskBook; 
+        if(button.innerHTML.includes('open')) {
+            if(document.querySelector('.desk').innerHTML == "") {
+                document.querySelector('.desk').innerHTML = deskBook;
+                formInit();
+            }
+        } 
         if(button.innerHTML.includes('remove')) document.querySelector('.desk').innerHTML = "";
         if(button.innerHTML.includes('plus')) {
             addBook();
@@ -20,20 +23,25 @@ deskButtons.forEach((button) => {
     });
 });
 
-//add event listeners to the form to update the left side of it. Add it in a function so that it can be called each time a new form is generated
-//(on open button or book press)
+//1: We need to add an event listener to each book as they're shelved, that allows them to fill the desk book form based on the data stored in them
+//2: Another event listener must be added to the banner of each book that alters the read or unread status of both the banner and the linked object
+//5: find a way to prevent the form from submitting if parameters are not met
+//6: Add animation
+//7: Add media query
 
-//When you add a book to the shelf, save the innerHTML of the form and store it in the object, so it can be called when the book is clicked
-
-//Clicking a book on the shelf pulls the book out of the array and saves in a tempbook.
-
-//shelve function generates the visual shelf based on the book objects in the current array
-
-//add a class to the book object generated that changes its color depending on the genre
-
-//Add an event listener to the banner on the book that swaps the color of the banner from red to green
-
-
+function formInit(book){
+    document.querySelectorAll("input, select").forEach((input) =>{
+        input.addEventListener('input', ()=>{
+            if(input.id == 'author') document.querySelector('.author').innerHTML = input.value;
+            if(input.id == 'title') document.querySelector('.title').innerHTML = input.value;
+            if(input.id == 'pgNumber') document.querySelector('h3').innerHTML = input.value + " pages";
+            if(input.id == 'genre'){
+                document.querySelector('.book-open').className = "book-open";
+                document.querySelector('.book-open').classList.add(input.value);
+            }
+        });
+    });
+};
 
 function addBook(){
     if (Books.length < 10){
@@ -49,7 +57,42 @@ function addBook(){
 }
 
 function shelve(shelf){
-    //remember to add the .last class to the last book on the shelf, if the number of the books in the array are less than 10
+    if(shelf == Books){
+        document.querySelector('.shelf-track').innerHTML = "";
+        for(let i = 0; i < shelf.length; i++){
+            document.querySelector('.shelf-track').innerHTML += shelfBook;
+            var currBook = document.querySelector('.shelf-track>.book:nth-child('+(i+1)+')');
+            currBook.querySelector('h2').innerHTML = shelf[i].title;
+            currBook.classList.add(shelf[i].genre);
+            if (i+1 == shelf.length && shelf.length < 10) currBook.classList.add('last');
+        };
+        document.querySelectorAll('.shelf-track>.book').forEach((book) =>{
+            book.addEventListener('click', ()=>{
+                //Get the index of an item in an array, based on a match between the index item's NAME attribute and the h2 of the 'book' element.
+                //If the desk is empty, use the index to populate the form data
+                //then use the index of the item to remove it from the array.
+                shelve(shelf); //finally, update the shelf.
+            });
+        });
+    }
+    else{
+        document.querySelector('.shelf-track-2').innerHTML = "";
+        for(let i = 0; i < shelf.length; i++){
+            document.querySelector('.shelf-track-2').innerHTML += shelfBook;
+            var currBook = document.querySelector('.shelf-track-2>.book:nth-child('+(i+1)+')');
+            currBook.querySelector('h2').innerHTML = shelf[i].title;
+            currBook.classList.add(shelf[i].genre);
+            if (i+1 == shelf.length && shelf.length < 10) currBook.classList.add('last');
+        }
+        document.querySelectorAll('.shelf-track-2>.book').forEach((book) =>{
+            book.addEventListener('click', ()=>{
+                //Get the index of an item in an array, based on a match between the index item's NAME attribute and the h2 of the 'book' element.
+                //If the desk is empty, use the index to populate the form data
+                //then use the index of the item to remove it from the array.
+                shelve(shelf);//finally, update the shelf.
+            });
+        });
+    };
 }
 
 function book(title, author, num, genre, read){
